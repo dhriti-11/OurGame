2804c0060e2c75a6a6eaf6dd8380b7284900a8dd
dhriti-11 <justsat478@gmail.com>
2026-03-09 15:05:12 -0400

    dhriti commit

--- YOURGAMENAMEHERE/YOURGAMENAMEHERE/src/game/Asteroid.java
+++ YOURGAMENAMEHERE/YOURGAMENAMEHERE/src/game/Asteroid.java
@@ -1,10 +1,23 @@
 class Asteroid extends Polygon
 {
+
     public Asteroid(Point[] shape, Point position, double rotation) {
         super(shape, position, rotation);
     }
 
     public void paint(Graphics brush) {
+
+        Point[] asteroidPoints = this.getPoints();
+        int[] x = new int[asteroidPoints.length];
+        int[] y = new int[asteroidPoints.length];
+
+        for(int i=0 ;i< asteroidPoints.length; i++)
+        {
+            x[i] = (int)asteroidPoints[i];
+            y[i] = (int)asteroidPoints[i];
+        }
+
+        brush.fillPolygon(x,y,asteroidPoints.length);
 
     }
 
