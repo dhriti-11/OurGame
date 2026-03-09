3329523fb858845e9a09545e3fd10c1afb47f26a
dhriti-11 <justsat478@gmail.com>
2026-03-09 14:52:30 -0400

    dhriti commit

---
+++ YOURGAMENAMEHERE/YOURGAMENAMEHERE/src/game/Asteroid.java
@@ -0,0 +1,11 @@
+class Asteroid extends Polygon
+{
+    public Asteroid(Point[] shape, Point position, double rotation) {
+        super(shape, position, rotation);
+    }
+
+    public void paint(Graphics brush) {
+
+    }
+
+}
